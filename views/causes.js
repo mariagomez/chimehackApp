1f6f442585e1f946180ca2d2bbe4ba17700e05ea
Maria Gomez <mgomez@thoughtworks.com>
2013-12-06 21:52:02 -0800

    Maria. Hooked up the back end

--- views/causes.js
+++ views/causes.js
@@ -1,14 +1,18 @@
-MyApp.causesData =[
-	{title: 'Girls education in Afghanistan', category: "education", chime_reason: "because my edcation allows me to", link: "www.google.com" }, { title: 'The unacceptability of violenece against women', category: "justice", chime_reason: "because", link: "www.google.com" },
-	{title: 'Access to clean water', category: "health", chime_reason: "because", link: "www.google.com" },
-	{title: 'The unacceptability of violenece against women', category: "justice", chime_reason: "because", link: "www.google.com" }
-];
 
 MyApp.causes = function() {
 
 	var viewModel = {
-		todayCauses: MyApp.causesData
+        dataSource: new DevExpress.data.DataSource({
+            load: function(loadOptions) {
+                return $.getJSON('http://chimeapp.herokuapp.com/causes');
+            },
+            map: function(item) {
+                return {
+                    title: item.title
+                };
+            }
+        })
     };
-
     return viewModel;
 };
+
